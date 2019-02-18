6bdd0916087fd16b4d929336ba28145ba912e435
Anas Idiab <candfa2660@gmail.com>
2019-02-18 11:30:33 +0100

    Proper Runge-Kutta implementation

--- laneemden1.cpp
+++ laneemden1.cpp
@@ -1,5 +1,8 @@
 #define PLOT true
+#define ERROR errorf(ξ + dξ, θ)
 #define ½ (1.0/2.0)*
+#define N 5
+#define DKSI 0.4
 #include <cmath>
 #include <fstream>
 #include <assert.h>
@@ -8,9 +11,6 @@ typedef double real;
 typedef double ℝ;
 
 void WritePoint(ofstream& outfile, ℝ x, ℝ y, ℝ dy);
-
-
-ℝ n = 1;
 
 ofstream  eulercsv;
 ofstream  rungekuttacsv;
@@ -27,48 +27,55 @@ ofstream  rungekuttacsv;
 
 
 ℝ dv(ℝ ξ, ℝ θ){
-  return -((ξ*ξ)*pow(θ, n));
+  return -((ξ*ξ)*pow(θ, N));
 }
 
 ℝ dθ(ℝ ξ, ℝ v) {
   return v*(1.0/(ξ*ξ));
 }
 
+#if N == 1
+ℝ errorf(ℝ ξ, ℝ θ) { return θf1(ξ) - θ; }
+#elif N == 5
+ℝ errorf(ℝ ξ, ℝ θ) { return θf5(ξ) - θ; }
+#else
+ℝ errorf(ℝ ξ, ℝ θ) { return 0; }
+#endif
 
 void EulerIntegration(){
-  ℝ θ = 1.0, ξ = 0, dξ = 0.05, ξmax = 10.0, v = 0, error = 0;
-  WritePoint(eulercsv, ξ, θ, θf1(ξ + dξ) - θ);
-  error += abs(θf1(ξ + dξ) - θ);
-  for (ξ = 5*dξ; ξ < ξmax; ξ += dξ) {
+  ℝ θ = 1.0, ξ = 0, dξ = DKSI, ξmax = 10.0, v = 0, error = 0;
+  WritePoint(eulercsv, ξ, θ, ERROR);
+  error += abs(ERROR);
+  for (ξ = dξ; ξ < ξmax; ξ += dξ) {
     v += dv(ξ, θ) * dξ;
     θ += dθ(ξ, v) * dξ;
-    WritePoint(eulercsv, ξ, θ, θf1(ξ)-θ);
-    error += abs(θf1(ξ + dξ) - θ);
+    WritePoint(eulercsv, ξ, θ, ERROR);
+    error += abs(ERROR);
   }
   printf("Euler error: %f\n", error);
 }
 
 
 void RungeKuttaFourthOrderIntegration() {
-  ℝ θ = 1.0, ξ = 0, dξ = 0.05, ξmax = 10.0, v = 0, k1, k2, k3, k4, error = 0;
-  WritePoint(rungekuttacsv, ξ, θ, θf1(ξ + dξ) - θ);
-  error += abs(θf1(ξ + dξ) - θ);
+  ℝ θ = 1.0, ξ = 0, dξ = DKSI, ξmax = 10.0, v = 0, error = 0;
+  ℝ k1, k2, k3, k4, kv1, kv2, kv3, kv4;
+  WritePoint(rungekuttacsv, ξ, θ, ERROR);
+  error += abs(ERROR);
   for (ξ = dξ; ξ < ξmax; ξ += dξ) {
-    // Runge-Kutta step for v
-    k1 = dξ * dv(ξ, θ);
-    k2 = dξ * dv(ξ + ½ dξ, θ + ½ k1);
-    k3 = dξ * dv(ξ + ½ dξ, θ + ½ k2);
-    k4 = dξ * dv(ξ + dξ, θ + k3);
-    v += (k2 + k3 + ½ (k1 + k4)) / 3.0;
-
-    // Runge-Kutta step for θ
+    // Runge-Kutta step for v and theta
+    kv1 = dξ * dv(ξ, θ);
     k1 = dξ * dθ(ξ, v);
-    k2 = dξ * dθ(ξ + ½ dξ, v + ½ k1);
-    k3 = dξ * dθ(ξ + ½ dξ, v + ½ k2);
-    k4 = dξ * dθ(ξ + dξ, v + k3);
+    kv2 = dξ * dv(ξ + ½ dξ, θ + ½ k1);
+    k2 = dξ * dθ(ξ + ½ dξ, v + ½ kv1);
+    kv3 = dξ * dv(ξ + ½ dξ, θ + ½ k2);
+    k3 = dξ * dθ(ξ + ½ dξ, v+ ½ kv2);
+    kv4 = dξ * dv(ξ + dξ, θ + k3);
+    k4 = dξ * dθ(ξ + dξ, v + kv3);
+    v += (kv2 + kv3 + ½ (kv1 + kv4)) / 3.0;
     θ += (k2 + k3 + ½ (k1 + k4)) / 3.0;
-    WritePoint(rungekuttacsv, ξ, θ, θf1(ξ)-θ);
-    error += abs(θf1(ξ + dξ) - θ);
+
+    WritePoint(rungekuttacsv, ξ, θ, ERROR);
+    error += abs(ERROR);
   }
   printf("Runge-Kutta error: %f\n", error);
 }
