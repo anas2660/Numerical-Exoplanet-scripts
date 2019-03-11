07f33a3064a8121e5a5f2588c81e4cb76ce49e28
Morten R. Bastholm <local@local.com>
2019-03-11 14:16:09 +0100

    Added color support and changed input method

--- ternaryplot.py
+++ ternaryplot.py
@@ -1,7 +1,10 @@
 #!/bin/env python3
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 import ternary
 import sys
+
+colors = ["r", "y", "g", "b", "g", "y", "r"]
 
 def plot(data, save="", show=False, figure=None, **kwargs):
     
@@ -10,13 +13,14 @@ def plot(data, save="", show=False, figure=None, **kwargs):
     else:
         fig, tax = figure
     
+    gammaData = np.ones(len(data[:,0])) - data[:,0] - data[:,1]
+    
     # Draw Boundary and Gridlines
     tax.boundary(linewidth=1.0)
     tax.gridlines(multiple=0.1, color="black")
     
     tax.ticks(axis="lbr", multiple=0.1, linewidth=1.0, tick_formats = "%.1f", offset = 0.02)
     tax.ax.set_aspect("equal",adjustable='box')
-    #tax.clear_matplotlib_ticks()
     tax.ax.axis("off")
     
     # Set Axis labels and Title
@@ -24,25 +28,27 @@ def plot(data, save="", show=False, figure=None, **kwargs):
     tax.right_axis_label("$\\gamma$ (water)", offset= 0.16)
     tax.bottom_axis_label("$\\alpha$ (iron)", offset= 0.06)
 
-    tax.plot(data, **kwargs)
+    tax.plot(np.stack((data[:,0], gammaData, data[:,1]),axis=1), **kwargs)
     
     if save != "":
         fig.savefig(save, bbox_inches="tight")
     
-    
+
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("Too few arguments were passed.", file=sys.stderr)
         sys.exit(1)
     else:
-        fig = ternary.figure()
         for arg in sys.argv[1:]:
-            try:
-                data = numpy.genfromtxt(arg, delimiter=",")
-                plot(data, show=True, color="red", figure=fig)
-            except Exception as e:
-                print(e, file=sys.stderr)
-        fig[1].show()
+            fig = ternary.figure()
+            fig[1].set_title(arg, pad= 16)
+            for i in range(0,7):
+                try:
+                    data = np.genfromtxt("out/"+arg+str(i)+".csv", delimiter=",")
+                    plot(data, show=True, color=colors[i], figure=fig)
+                except Exception as e:
+                        print(e, file=sys.stderr)
+    plt.show()
 
 else:
     print("Program not called directly.", file=sys.stderr)
