8114290dd224d00ed12921a0d6c08aaa74da4cd3
Anas Idiab <candfa2660@gmail.com>
2019-03-14 22:18:03 +0100

    change from ln to log10

--- errplot.py
+++ errplot.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
-from numpy import log, genfromtxt
+from numpy import log10, genfromtxt
 ERR = genfromtxt('error.csv', delimiter=',')
-plt.plot(log(ERR[5:, 0]), log(ERR[5:, 1]), 'r', label="Euler")
-plt.plot(log(ERR[5:, 0]), log(ERR[5:, 2]), 'g', label="RK2")
-plt.plot(log(ERR[5:, 0]), log(ERR[5:, 3]), 'g', label="RK4")
+plt.plot(log10(ERR[5:, 0]), log10(ERR[5:, 1]), 'r', label="Euler")
+plt.plot(log10(ERR[5:, 0]), log10(ERR[5:, 2]), 'g', label="RK2")
+plt.plot(log10(ERR[5:, 0]), log10(ERR[5:, 3]), 'g', label="RK4")
 plt.grid()
-plt.xlabel('$ln(Steps)$')
-plt.ylabel('$ln(Error)$')
+plt.xlabel('$log_{10}(Steps)$')
+plt.ylabel('$log_{10}(Error)$')
 plt.title('Error convergence')
 plt.legend()
 plt.savefig("out/error.pdf", bbox_inches='tight', pad_inches=0)
