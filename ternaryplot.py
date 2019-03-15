cc3070c04a6020975f0259cbd776ea18b4cd22f6
Anas Idiab <candfa2660@gmail.com>
2019-03-15 11:12:29 +0100

    remove redundant else

--- ternaryplot.py
+++ ternaryplot.py
@@ -32,20 +32,19 @@ if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("Too few arguments were passed.", file=sys.stderr)
         sys.exit(1)
-    else:
-        for arg in sys.argv[1:]:
-            fig = ternary.figure()
-            plotsetup(fig)
-            for i in range(0, 7):
-                try:
-                    data = np.genfromtxt("out/"+arg+str(i)+".csv",
-                                         delimiter=",")
-                    plot(data, color=colors[i], figure=fig)
-                except Exception as e:
-                        print(e, file=sys.stderr)
-            fig[1]._redraw_labels()
-            plt.savefig("out/"+arg+".pdf", bbox_inches='tight',
-                        pad_inches=0)
+    for arg in sys.argv[1:]:
+        fig = ternary.figure()
+        plotsetup(fig)
+        for i in range(0, 7):
+            try:
+                data = np.genfromtxt("out/"+arg+str(i)+".csv",
+                                     delimiter=",")
+                plot(data, color=colors[i], figure=fig)
+            except Exception as e:
+                print(e, file=sys.stderr)
+        fig[1]._redraw_labels()
+        plt.savefig("out/"+arg+".pdf", bbox_inches='tight',
+                    pad_inches=0)
     plt.show()
 else:
     print("Program not called directly.", file=sys.stderr)
